644661ecf2a83ebf7d74726b7db96941ac23b2bd
Stephen Hurd <deuce@synchro.net>
2026-02-01 04:50:42 +0000

    More syntax fixins

--- klippy/extras/gcode_button.py
+++ klippy/extras/gcode_button.py
@@ -51,9 +51,9 @@ class GCodeButton:
                     self.gcode.run_script_from_command(
                         "M117 Please calibrate Motor A!"
                     )
-                elif self.triggered_time > 0.25 and
+                elif (self.triggered_time > 0.25 and
                   self.triggered_time < 0.8 and
-                  not self.printer.in_shutdown_state:
+                  not self.printer.in_shutdown_state):
                     self.printer.invoke_shutdown(
                         "Error has occurred with Motor A"
                     )
@@ -63,9 +63,9 @@ class GCodeButton:
                     self.gcode.run_script_from_command(
                         "M117 Please calibrate Motor B!"
                     )
-                elif self.triggered_time > 0.25 and
+                elif (self.triggered_time > 0.25 and
                   self.triggered_time < 0.8 and
-                  not self.printer.in_shutdown_state:
+                  not self.printer.in_shutdown_state):
                     self.printer.invoke_shutdown(
                         "Error has occurred with Motor B"
                     )
@@ -75,9 +75,9 @@ class GCodeButton:
                     self.gcode.run_script_from_command(
                         "M117 Please calibrate Motor C!"
                     )
-                elif self.triggered_time > 0.25 and
+                elif (self.triggered_time > 0.25 and
                   self.triggered_time < 0.8 and
-                  not self.printer.in_shutdown_state:
+                  not self.printer.in_shutdown_state):
                     self.printer.invoke_shutdown(
                         "Error has occurred with Motor C"
                     )
--- klippy/extras/virtual_sdcard.py
+++ klippy/extras/virtual_sdcard.py
@@ -181,8 +181,9 @@ class VirtualSD:
             filename = filename[1:]
         self._load_file(gcmd, filename)
     # Start FLSUN Changes
-    cmd_POWER_LOSS_RESTART_PRINT_help =
+    cmd_POWER_LOSS_RESTART_PRINT_help = (
         "Restart print after power loss and power on"
+    )
     def cmd_POWER_LOSS_RESTART_PRINT(self, gcmd):
         filename = gcmd.get("FILENAME")
         fileposition = gcmd.get("FILEPOSITION")
