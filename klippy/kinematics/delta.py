6d262322b4dd5365c709a527c47fb0728f79b65d
Stephen Hurd <deuce@synchro.net>
2026-02-01 05:37:28 +0000

    Try loading instead

--- klippy/kinematics/delta.py
+++ klippy/kinematics/delta.py
@@ -51,7 +51,7 @@ class DeltaKinematics:
         print_radius = config.getfloat("print_radius", radius, above=0.0)
         arm_length_a = stepper_configs[0].getfloat("arm_length", above=radius)
         # Start FLSUN Changes
-        gcode_move = self.printer.lookup_object('gcode_move')
+        gcode_move = self.printer.load_object(config, 'gcode_move')
         x_size_offset, y_size_offset = gcode_move.get_xy_size_offset()
         if x_size_offset > 0 or y_size_offset > 0:
             print_radius = print_radius*(1 + max(x_size_offset, y_size_offset))
